2d940d08f1a39027bfbd14f0fe57e6769e49e170
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2025-05-15 11:45:18 -0300

    iniciação

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,50 @@
+"use client";
+
+import styles from "./profile.module.css";
+import { Button, Card, Flex, Typography, Skeleton } from "antd";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Profile() {
+  return (
+    <Card hoverable className={styles.card}>
+      <Flex justify="space-between">
+        <div className={styles.imageContainer}>
+          <Image
+            src="/images/anaju.png"
+            alt="Nome do Caboclo"
+            fill
+            className={styles.image}
+          />
+        </div>
+        <Flex vertical align="flex-end" justify="space-around">
+          <div>
+            <Typography.Title level={3}>Mona LisaBean</Typography.Title>
+            <Typography.Title level={5} type="success">
+              Mockup Front1 Exam
+            </Typography.Title>
+            <Typography.Paragraph>
+              Projeto desenvolvido usando:
+            </Typography.Paragraph>
+            <ul className={styles.list}>
+              <li>NextJS</li>
+              <li>Axios</li>
+              <li>AntD</li>
+              <li>SessionStorage</li>
+              <li>Toastify</li>
+              <li>CSS Modules</li>
+              <li>Hook</li>
+              <li>PreLoad</li>
+              <li>PreFetch</li>
+              <li>Link / Next</li>
+              <li>Image / Next</li>
+            </ul>
+          </div>
+          <Link href="/alunos" prefetch>
+            <Button type="primary">Acessar minha API GET via Axios</Button>
+          </Link>
+        </Flex>
+      </Flex>
+    </Card>
+  );
+}
